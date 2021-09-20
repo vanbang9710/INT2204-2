05f4abd82d8ee97288c62df5804a92ac35d911a4
BeztDonkey <52906188+vanbang9710@users.noreply.github.com>
2021-09-20 17:30:38 +0700

    Create Solution.java

---
+++ HelloWorld/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(Solution.printHello());
+    }
+}
