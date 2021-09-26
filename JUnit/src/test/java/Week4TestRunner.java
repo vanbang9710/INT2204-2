34f5573195a5fdbcb564792a06c03906e2189f84
BeztDonkey <52906188+vanbang9710@users.noreply.github.com>
2021-09-27 00:12:42 +0700

    Week 2

---
+++ JUnit/src/test/java/Week4TestRunner.java
@@ -0,0 +1,13 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class Week4TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Week4Test.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println("Result=="+result.wasSuccessful());
+    }
+}
